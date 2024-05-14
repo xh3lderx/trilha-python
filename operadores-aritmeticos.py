bd24d2625b40eb900986f162b54e8e69cc8228b6
oheldertavares <x3lderx@outlook.com.br>
2024-05-14 00:31:54 -0300

    operadores aritmeticos

---
+++ operadores-aritmeticos.py
@@ -0,0 +1,25 @@
+#adição
+print(1+7)
+print(2+8)
+print(12+54)
+#subtração
+print(51-12)
+print(45-21)
+print(21-12)
+#multiplicação
+print(4*4)
+print(10*5)
+print(5*5)
+#divisão normal e inteira
+print(10/2)
+print(20/4)
+print(10//2)
+print(20//4)
+#modulo o resto da divisão
+print(10%3)
+print(20%5)
+print(10%2)
+#exponeciação, numero multiplicado por ele mesmo
+print(2**3)
+print(5**6)
+print(4**8)
