90f740b731b761949e447310e1490745526286ba
oheldertavares <xh3lderx@outlook.com.br>
2024-06-18 04:16:31 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,28 @@
+nome = 'Helder'
+idade = 26
+cidade = 'Guaruja'
+estado = 'SP'
+nascimento = '13/06/1992'
+relacionamento = 'namorando'
+namorada = 'Claudia'
+altura = 1.87
+peso = 130.0
+escolaridade = 'ensino medio completo'
+ensino_superior = 'graduando'
+materia = 'computação em nuvem'
+mae = "Adriana Amaro Costa dos Santos"
+pai = 'José Carlos dos Santos'
+irmao = 'Helber Costa dos Santos'
+irma = 'Andrezza Costa dos Santos'
+empresa = 'Terracom'
+salario = 1453.57
+cargo = 'Gari'
+
+print(f"Olá, meu nome é {nome}, tenho {idade} anos. Nascido em {cidade}/{estado}")
+print(f'Atualmente meu relacionamento é {relacionamento} com {namorada}')
+print(f'Atualmente meço {altura}, meu peso é {peso}')
+print(f"A minha escolaridade é {escolaridade}")
+print(f'Estou me {ensino_superior} em {materia}')
+print(f"Minha mãe é {mae} e meu pai é {pai}")
+print(f'Tenhos 2 irmãos um homem e uma mulher, {irmao} e {irma}')
+print(f'Atualmente trabalho na empresa {empresa}, com um salario de {salario}, no cargo de {cargo}')
